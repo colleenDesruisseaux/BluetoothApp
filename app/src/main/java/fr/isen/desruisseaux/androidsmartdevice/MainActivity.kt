49507ae3a7ca5057fcec78f76649776ad94550d9
colleenDesruisseaux <colleenDesruisseaux>
2023-04-06 21:36:58 +0200

    design page accueil OK

--- app/src/main/java/fr/isen/desruisseaux/androidsmartdevice/MainActivity.kt
+++ app/src/main/java/fr/isen/desruisseaux/androidsmartdevice/MainActivity.kt
@@ -2,6 +2,7 @@ package fr.isen.desruisseaux.androidsmartdevice
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import fr.isen.desruisseaux.androidsmartdevice.databinding.ActivityMainBinding
@@ -9,6 +10,7 @@ import fr.isen.desruisseaux.androidsmartdevice.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var cptBut = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +27,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.regles.setOnClickListener {
-            binding.textRegles.isVisible = true
-            binding.textRegles.text=" * Scanner et détecter les appareils\n\n * Sélectionner un appareil\n\n * Connexion établie\n\n * Click sur une LED pour l'allumer\n   sur la carte STM32"
+            cptBut++
+            if (cptBut%2 == 1){
+                binding.textRegles.isVisible=false
+                binding.regles.text="Aide"
+            }
+            else{
+                binding.textRegles.isVisible=true
+                binding.textRegles.text=" * Scanner et détecter les appareils\n\n * Sélectionner un appareil\n\n * Connexion établie\n\n * Click sur une LED pour l'allumer\n   sur la carte STM32"
+                binding.regles.text="X"
+            }
         }
     }
 }
